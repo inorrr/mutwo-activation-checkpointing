aad89df94c3d96f96b80a4b20733f3ab6d429977
Yinuo Zhao <yinuozhao@hsph.harvard.edu>
2026-04-27 23:20:32 -0400

    Initial commit

---
+++ benchmarks.py
@@ -0,0 +1,125 @@
+import importlib
+from typing import Any, Dict, List
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.fx as fx
+from torch.testing._internal.distributed._tensor.common_dtensor import (
+    ModelArgs,
+    Transformer,
+    )
+from torchvision.models import resnet18, resnet50
+from graph_prof import GraphProfiler
+from graph_tracer import SEPFunction, compile
+
+
+model_names: List[str] = [
+    "Transformer",
+    "Resnet18",
+    "Resnet50",
+]
+
+model_batch_sizes: Dict[str, int] = {
+    "Transformer": 4,
+    "Resnet18": 16,
+    "Resnet50": 4,
+}
+
+
+class Experiment:
+    def __init__(self, model_name: str, batch_size: int, extra_args=[]):
+        assert model_name in model_names, f"Model {model_name} not found in model names {model_names}"
+        dev = torch.device("cuda")
+        self.model_name = model_name
+        self.batch_size = batch_size
+
+        if self.model_name == "Transformer":
+
+            vocab_size = 2048
+            bsz, seq_len = self.batch_size, 256
+            with torch.device(dev):
+                model_args = ModelArgs(
+                    n_layers=8,
+                    n_heads=4,
+                    vocab_size=vocab_size,
+                    max_seq_len=seq_len,
+                    dropout_p=0.1,
+                )
+                self.model = Transformer(model_args)
+            src = torch.randint(0, vocab_size, (bsz, seq_len), device=dev)
+            tgt = torch.randint(0, vocab_size, (bsz, seq_len), device=dev)
+            self.example_inputs = (src, tgt)
+
+            def transformer_train_step(
+                model: nn.Module, optim: optim.Optimizer, example_inputs: Any
+            ):
+                loss = self.loss_fn(model(example_inputs[0]), example_inputs[1])
+                loss = SEPFunction.apply(loss)
+                loss.backward()
+                optim.step()
+                optim.zero_grad()
+
+            self.train_step = transformer_train_step
+            self.optimizer = optim.Adam(self.model.parameters(), lr=1e-2, fused=True, capturable=True)
+
+        elif self.model_name in ["Resnet18", "Resnet50"]:
+            inp = torch.randn(self.batch_size, 3, 224, 224, device=dev)
+            num_classes = 10
+            target = torch.randint(0, num_classes, (self.batch_size,), device=dev)
+            self.example_inputs = (inp, target)
+            with torch.device(dev):
+                self.model = resnet18() if self.model_name == "Resnet18" else resnet50()
+
+            def resnet_train_step(
+                model: nn.Module, optim: optim.Optimizer, example_inputs: Any
+            ):
+                loss = self.loss_fn(model(example_inputs[0]), example_inputs[1])
+                loss = SEPFunction.apply(loss)
+                loss.backward()
+                optim.step()
+                optim.zero_grad()
+
+            self.optimizer = optim.Adam(self.model.parameters(), lr=1e-2, fused=True, capturable=True)
+            self.train_step = resnet_train_step
+
+    def loss_fn(self, logits: torch.Tensor, targets: torch.Tensor):
+        return F.cross_entropy(
+            logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1
+        )
+
+    def init_opt_states(self):
+        for param in self.model.parameters():
+            if param.requires_grad:
+                param.grad = torch.rand_like(param)
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+
+    def graph_transformation(self, gm: fx.GraphModule, args: Any) -> fx.GraphModule:
+        print(gm.graph.print_tabular())
+        warm_up_iters, profile_iters = 2, 3
+        graph_profiler = GraphProfiler(gm)
+
+        with torch.no_grad():
+            for _ in range(warm_up_iters):
+                graph_profiler.run(*args)
+            graph_profiler.reset_stats()
+
+            for _ in range(profile_iters):
+                graph_profiler.run(*args)
+            graph_profiler.aggregate_stats()
+            graph_profiler.print_stats()
+
+        return gm
+
+    def run(self):
+        self.train_step(self.model, self.optimizer, self.example_inputs)
+        print("Successful.")
+
+
+if __name__ == "__main__":
+    exp = Experiment(model_names[1], model_batch_sizes[model_names[1]])
+    exp.init_opt_states()
+    compiled_fn = compile(exp.train_step, exp.graph_transformation)
+    compiled_fn(exp.model, exp.optimizer, exp.example_inputs)
---
+++ starter_code.py
@@ -0,0 +1,127 @@
+import logging
+import os
+from functools import wraps
+from typing import Any
+
+import torch
+import torch.fx as fx
+import torch.multiprocessing as mp
+import torch.nn as nn
+
+from graph_prof import GraphProfiler
+from graph_tracer import SEPFunction, compile
+
+# This is the dummy model that is for use in starter code. But we will
+# experiment with Resnet and Transformer model.
+
+
+class DummyModel(nn.Module):
+    def __init__(self, layers: int, dim: int):
+        super().__init__()
+        modules = []
+        for _ in range(layers):
+            modules.extend([nn.Linear(dim, dim), nn.ReLU()])
+        self.mod = nn.Sequential(*modules)
+
+    def forward(self, x):
+        return self.mod(x)
+
+
+# We wrap the loss with a separator function to call a
+# dummy function 'SEPFunction', which is the separator function, that will call
+# an identity operator at the end of the forward pass. This identity operator
+# will get recorded in the computational graph and will inform you where the
+# backward pass ends.
+
+
+# This is the train_step function that takes in a model, optimizer and an input
+# mini batch and calls the forward pass, loss function and the optimizer step. A
+# computational graph corresponding to a train_step will be captured by the
+# compiler.
+
+
+def train_step(
+    model: torch.nn.Module, optim: torch.optim.Optimizer, batch: torch.Tensor
+):
+    loss =  model(batch).sum()
+    loss = SEPFunction.apply(loss)
+    loss.backward()
+    optim.step()
+    optim.zero_grad()
+
+
+# Below is a user defined function that accepts a graph module and arguments of
+# used to run the graph. You can essentially do any operation, graph
+# modification, profiling etc. inside this function. Subsequent to modifications
+# or graph analysis, the function expects you to return the modified graph back.
+# In the given example, we just print the graph, and then initilize the graph
+# profiler. The graph profiler extends the class fx.Interpreter, that allows you
+# to run the graph node by node, more explanation in graph_prof.py.
+
+
+def graph_transformation(gm: fx.GraphModule, args: Any) -> fx.GraphModule:
+    print(gm.graph)
+    graph_profiler = GraphProfiler(gm)
+    warm_up_iters, profile_iters = 2, 3
+    with torch.no_grad():
+        for _ in range(warm_up_iters):
+            graph_profiler.run(*args)
+        graph_profiler.reset_stats()
+        for _ in range(profile_iters):
+            graph_profiler.run(*args)
+    graph_profiler.aggregate_stats()
+    graph_profiler.print_stats()
+    return gm
+
+
+# We first initialize the model, pass it to the wrapper model, then create a
+# random input mini-batch and initilize the optimizer. We then call the compile
+# function that takes in two arguments, a train_step function and a
+# graph_transformation function. The train_step function is the one that will be
+# traced by the compiler and a computational graph for the same will be created.
+# This computational graph is then passed to the graph_transformation function
+# to do any graph profiling, modifications and optimizations. This modified
+# graph is stored and will be returned as the compiled function. In essence we
+# do the following inside the compile function:
+
+# def compile (train_step, graph_transformation):
+#     @wraps(train_step)
+#     def inner(*args, **kwargs):
+#         if not_compiled:
+#             original_graph, input_args = graph_tracer(train_step)
+#             modified_graph = graph_transformation(original_graph, input_args)
+#         output = modified_graph(*args, **kwargs)
+#         return output
+#     return inner
+
+
+def experiment():
+    logging.getLogger().setLevel(logging.DEBUG)
+    torch.manual_seed(20)
+    batch_size = 1000
+    layers = 10
+    dim = 100
+    num_iters = 5
+
+    device_str = 'cuda:0'
+    model = DummyModel(dim=dim, layers=layers).to(device_str)
+    batch = torch.randn(batch_size, dim).to(device_str)
+    optim = torch.optim.Adam(
+        model.parameters(), lr=0.01,
+        foreach=True,  # fused=True,
+        capturable=True
+    )
+
+    for param in model.parameters():
+        if param.requires_grad:
+            param.grad = torch.rand_like(param, device=device_str)
+
+    optim.step()
+    optim.zero_grad()
+
+    compiled_fn = compile(train_step, graph_transformation)
+    compiled_fn(model, optim, batch)
+
+
+if __name__ == "__main__":
+    experiment()
